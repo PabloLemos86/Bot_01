0f1eca274a2178f6ca8312364be7f1fdb51fc1a7
PabloLemos86 <124652147+PabloLemos86@users.noreply.github.com>
2025-08-19 20:28:34 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# TOKEN do Bot (coloque o seu aqui)
+TOKEN = "8023106816:AAEB5QURW63jmPMZOntDxf-JcJzvWpflAV8"
+
+# Quantidade padrão de licitações retornadas
+DEFAULT_QTD = 5
+
+# Regiões e estados
+REGIOES = {
+    "Norte": ["AC", "AP", "AM", "PA", "RO", "RR", "TO"],
+    "Nordeste": ["AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE"],
+    "Centro-Oeste": ["DF", "GO", "MT", "MS"],
+    "Sudeste": ["ES", "MG", "RJ", "SP"],
+    "Sul": ["PR", "RS", "SC"],
+    "Nacional": []
+}
